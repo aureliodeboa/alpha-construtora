45a12cba4d01d463604e83001b7b01ce980fcd31
Aurelio Ribeiro <53971991+aureliodeboa@users.noreply.github.com>
2024-11-29 15:39:44 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import { ArrowRight, Shield, FileText, Users } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
